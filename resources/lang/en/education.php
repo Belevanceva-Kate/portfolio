7a86c52a9516d12d9ada1863e5731d16a52e7aa2
Kate <belekate2711@gmail.com>
2020-05-09 21:21:51 +0300

    update education section

--- resources/lang/en/education.php
+++ resources/lang/en/education.php
@@ -16,21 +16,23 @@ return [
     'list'  => [
 
         'master' => [
-            'date' => '2018 – 2019',
-            'name' => 'Pryazovskyi State Technical University',
-            'info' => 'Master Degree'
+            'heading'   => 'Master Degree',
+            'area'      => 'Computer Science. Information Systems and Technologies',
+            'name'      => 'Pryazovskyi State Technical University',
+            'date'      => '2018 – 2019'
         ],
 
         'bachelor' => [
-            'date' => '2014 – 2018',
-            'name' => 'Pryazovskyi State Technical University',
-            'info' => 'Bachelor Degree'
+            'heading'   => 'Bachelor Degree',
+            'area'      => 'Computer Science',
+            'name'      => 'Pryazovskyi State Technical University',
+            'date'      => '2014 – 2018'
         ],
 
         'school' => [
-            'date' => '2003 – 2014',
-            'name' => 'The educational complex "School-Lyceum of Information Technology №69"',
-            'info' => 'School'
+            'heading'      => 'School',
+            'name'      => 'The educational complex "School-Lyceum of Information Technology №69"',
+            'date'      => '2003 – 2014'
         ]
     ]
 
--- resources/views/components/education.blade.php
+++ resources/views/components/education.blade.php
@@ -1,24 +1,35 @@
 @if(!empty($education))
     <div class="education section">
 
-        @if (!empty(trans('education.title')))
+        @if (Lang::has('education.title') && !empty(Lang::get('education.title')))
             <h3 class="section__title">{{ __('education.title') }}</h3>
         @endif
 
         <div class="section__content">
             <ul class="education__list">
                 @foreach($education as $item)
-                    @if (!empty(trans('education.list.' . $item . '.name')))
-                        <li class="education__item">
-                            @if (!empty(trans('education.list.' . $item . '.info')))
-                                <p class="education__info">{{ __('education.list.' . $item . '.info') }}</p>
-                            @endif
+                    <li class="education__item">
+                        @if (Lang::has('education.list.' . $item . '.heading') &&
+                            !empty(Lang::get('education.list.' . $item . '.heading')))
+                            <p class="education__info">{{ __('education.list.' . $item . '.heading') }}</p>
+                        @endif
+                        @if (Lang::has('education.list.' . $item . '.area') &&
+                            !empty(Lang::get('education.list.' . $item . '.area')))
+                            <p class="education__area">{{ __('education.list.' . $item . '.area') }}</p>
+                        @endif
+                        @if (Lang::has('education.list.' . $item . '.program') &&
+                            !empty(Lang::get('education.list.' . $item . '.program')))
+                            <p class="education__program">{{ __('education.list.' . $item . '.program') }}</p>
+                        @endif
+                        @if (Lang::has('education.list.' . $item . '.name') &&
+                            !empty(Lang::get('education.list.' . $item . '.name')))
                             <p class="education__name">{{ __('education.list.' . $item . '.name') }}</p>
-                            @if (!empty(trans('education.list.' . $item . '.date')))
-                                <time class="education_date">{{ __('education.list.' . $item . '.date') }}</time>
-                            @endif
-                        </li>
-                    @endif
+                        @endif
+                        @if (Lang::has('education.list.' . $item . '.date') &&
+                            !empty(Lang::get('education.list.' . $item . '.date')))
+                            <time class="education__date">{{ __('education.list.' . $item . '.date') }}</time>
+                        @endif
+                    </li>
                 @endforeach
             </ul>
         </div>
