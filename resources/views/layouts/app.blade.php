5ad0eb5e327def736aa378878e089199f743435e
Kate <belekate2711@gmail.com>
2020-07-09 00:26:20 +0300

    update print styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@if(isset($title)) {{ $title }} @else Resume @endif</title>
+        <title>@if(isset($title)) {{ $title }} @else Portfolio. Kateryna Bielievantseva @endif</title>
         <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/png">
         <link href="@if(isset($page)) {{ asset('css/' . $page . '.css') }} @else {{ asset('css/index.css') }} @endif" rel="stylesheet">
         <link rel="stylesheet" media="print" href="{{ asset('css/print.css') }}" />
