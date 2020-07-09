e690b14731be1855bc37a72ce1e38fe31f9f607d
Kate <belekate2711@gmail.com>
2020-07-09 11:05:17 +0300

    update google analytics on contacts

--- resources/views/snippets/contacts.blade.php
+++ resources/views/snippets/contacts.blade.php
@@ -8,7 +8,7 @@
                         <a
                             href="{{ config('contacts.' . $item . '.link') }}"
                             class="contacts__link"
-                            onclick="ga('send', 'event', 'button', 'click', 'contacts');"
+                            onСlick="ga('send', 'event', 'button', 'click', 'contacts');"
                             @if($item == 'location')
                                 target="_blank"
                             @endif
